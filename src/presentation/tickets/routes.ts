7bc947a9b85071ee4c7f0d7d70b84306e158e30b
ivan <ivanjv1234@gmail.com>
2024-07-15 00:28:05 -0600

    feat: tickets route and controller

---
+++ src/presentation/tickets/routes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { TicketsController } from "./controller";
+
+export class TicketsRoutes {
+
+    static get routes() {
+
+        const router = Router();
+        const ticketController = new TicketsController()
+
+        router.get('/', ticketController.getTickets)
+        router.get('/last', ticketController.getLastTicketNumber)
+        router.get('/pending', ticketController.pendingTickets)
+
+        router.post('/', ticketController.createTicket)
+
+        router.get('/draw/:desk', ticketController.drawTicket)
+        router.put('/done/:ticketId', ticketController.ticketFinished)
+
+        router.get('/working-on', ticketController.workingOn)
+        return router;
+    }
+ }
