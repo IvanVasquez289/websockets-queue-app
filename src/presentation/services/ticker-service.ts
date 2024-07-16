79add4979455d2fa19a92e850d1ed2b91c6de972
ivan <ivanjv1234@gmail.com>
2024-07-15 18:12:05 -0600

    feat: tickets showed on screen

--- src/presentation/services/ticker-service.ts
+++ src/presentation/services/ticker-service.ts
@@ -3,7 +3,7 @@ import { Ticket } from "../../domain/interfaces/ticket";
 
 export class TicketService {
 
-    private readonly _tickets: Ticket[] = [
+    public readonly _tickets: Ticket[] = [
         {id: UuidAdapter.v4(), number:1 ,createdAt: new Date(), done: false},
         {id: UuidAdapter.v4(), number:2 ,createdAt: new Date(), done: false},
         {id: UuidAdapter.v4(), number:3 ,createdAt: new Date(), done: false},
@@ -12,18 +12,24 @@ export class TicketService {
     ]
 
 
+    private readonly workingOnTickets: Ticket[] = [] 
+
+    public get last4WorkingOnTickets(): Ticket[] {
+        return this.workingOnTickets.slice(0, 4)
+    }
+ 
     public get pendingTickets(): Ticket[] {
         return this._tickets.filter(ticket => !ticket.handledAtDesk)
     }
 
-    public lastTicketNumber(): number {
+    public get lastTicketNumber(): number {
         return this._tickets.length > 0 ? this._tickets.at(-1)!.number : 0
     }
 
     public createTicket(): Ticket {
         const ticket = {
             id: UuidAdapter.v4(),
-            number: this.lastTicketNumber() + 1,
+            number: this.lastTicketNumber + 1,
             createdAt: new Date(),
             handledAtDesk: undefined,
             handledAt: undefined,
@@ -40,8 +46,11 @@ export class TicketService {
         const ticket = this._tickets.find(t => !t.handledAtDesk) // obtener el primer ticket que no este atendido
         if(!ticket) return {status: 'error', message: 'No hay tickets pendientes'}
 
+        
         ticket.handledAtDesk = desk
         ticket.handledAt = new Date()
+        
+        this.workingOnTickets.unshift({...ticket})
 
         //TODO: WS
 
