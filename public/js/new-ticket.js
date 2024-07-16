371d20a4456ff379141740be321219410f8fa866
ivan <ivanjv1234@gmail.com>
2024-07-15 22:33:47 -0600

    feat: new-ticket screen

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -1,3 +1,21 @@
-
+const currentTicketLbl = document.getElementById('lbl-new-ticket');
+const createTicketButton = document.querySelector('button');
 
 console.log('Nuevo Ticket HTML');
+
+async function getLastTicket() {
+    const response = await fetch('/api/tickets/last');
+    const lastTicket = await response.json();
+    currentTicketLbl.innerText = `Ticket ${lastTicket}`
+}
+
+getLastTicket();
+
+createTicketButton.addEventListener('click', async () => {
+    const response = await fetch('/api/tickets', {
+        method: 'POST'
+    });
+    const ticket = await response.json();
+    console.log(ticket);
+    currentTicketLbl.innerText = `Ticket ${ticket.number}`;
+})
