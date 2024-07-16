b6b02d5a437410a622b881b0e11c1dcde35ab205
ivan <ivanjv1234@gmail.com>
2024-07-15 18:20:31 -0600

    feat: initialize routes

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -3,7 +3,6 @@ import path from 'path';
 
 interface Options {
   port: number;
-  routes: Router;
   public_path?: string;
 }
 
@@ -14,13 +13,11 @@ export class Server {
   private serverListener?: any;
   private readonly port: number;
   private readonly publicPath: string;
-  private readonly routes: Router;
 
   constructor(options: Options) {
-    const { port, routes, public_path = 'public' } = options;
+    const { port, public_path = 'public' } = options;
     this.port = port;
     this.publicPath = public_path;
-    this.routes = routes;
     this.configure()
   }
 
@@ -33,7 +30,7 @@ export class Server {
     this.app.use( express.static( this.publicPath ) );
   
     //* Routes
-    this.app.use( this.routes );
+    // this.app.use( this.routes );
   
     //* SPA /^\/(?!api).*/  <== Únicamente si no empieza con la palabra api
     this.app.get( /^\/(?!api).*/, (req, res) => {
@@ -42,6 +39,9 @@ export class Server {
     });
   }
   
+  public setRoutes(router: Router) {
+    this.app.use( router );
+  }
   
   async start() {
 
